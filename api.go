450558a7b79bf2ae63247f2c768dbf43740c63af
Ayes <52951851+ayes-web@users.noreply.github.com>
2021-12-10 18:34:07 +0200

    Made a proper api

--- api.go
+++ api.go
@@ -16,22 +16,62 @@ func api(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 		return
 	}
 
+	switch r.URL.Path[4:] {
+	case "upload":
+		upload_image_api(w, r, db)
+	case "delete":
+		delete_image_api(w, r, db)
+	case "account/new_upload_token":
+		new_upload_token_api(w, r, db)
+	case "account/delete":
+		account_delete_api(w, r, db)
+	case "admin/create_user":
+		admin_create_user(w, r, db)
+	case "admin/delete_user":
+		admin_delete_user(w, r, db)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+// Deletes your account with images
+func account_delete_api(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	if !r.Form.Has("token") {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
-	switch r.FormValue("type") {
-	case "upload":
-		upload_api(w, r, db)
-	case "delete":
-		delete_image_api(w, r, db)
+	token := r.FormValue("token")
+
+	var id int
+	row := db.QueryRow("delete from accounts where token=$1 returning id", token)
+	if row.Scan(&id) == sql.ErrNoRows {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
 	}
 
+	rows, err := db.Query("select file_name from public.images WHERE file_owner=$1", id)
+	if err != nil { // Im guessing this happens when it gets no results
+		return
+	}
+
+	for rows.Next() {
+		var file_name string
+		rows.Scan(&file_name)
+
+		os.Remove("/app/data/" + file_name)
+	}
+
+	db.Exec("delete from public.images WHERE file_owner=$1", id)
 }
 
 func delete_image_api(w http.ResponseWriter, r *http.Request, db *sql.DB) {
-	upload_token := r.FormValue("token")
+	if !r.Form.Has("upload_token") || !r.Form.Has("file_name") {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	upload_token := r.FormValue("upload_token")
 	file_name := r.FormValue("file_name")
 
 	var token_result sql.NullString
@@ -59,28 +99,30 @@ func delete_image_api(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 		os.Remove("/app/data/" + file_name)
 	}
 
-	fmt.Fprintf(w, "Successfully deleted image")
+	fmt.Fprintln(w, "Successfully deleted image")
 }
 
-func upload_api(w http.ResponseWriter, r *http.Request, db *sql.DB) {
-	upload_token := r.FormValue("token")
-
-	var result sql.NullString
-	err := db.QueryRow(`SELECT id FROM public.accounts WHERE upload_token = $1`, upload_token).Scan(&result)
-	if err != nil { // This error occurs when the token is incorrect
-		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+func upload_image_api(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	if !r.Form.Has("upload_token") {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
-	if !result.Valid {
+	upload_token := r.FormValue("upload_token")
+
+	var result sql.NullString
+	err := db.QueryRow("SELECT id FROM public.accounts WHERE upload_token=$1", upload_token).Scan(&result)
+	if err != nil { // This error occurs when the token is incorrect
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	} else if !result.Valid {
 		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 		return
 	}
 
 	fileRaw, fileHeader, err := r.FormFile("file")
 	if err != nil { // This error occurs when user doesn't send anything on file
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		fmt.Println("no file")
+		http.Error(w, "No file provided", http.StatusBadRequest)
 		return
 	}
 
@@ -114,4 +156,22 @@ func upload_api(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 
 	http.Redirect(w, r, "https://"+r.Host+"/"+full_file_name, http.StatusFound)
 	fmt.Fprintln(w, "https://"+r.Host+"/"+full_file_name)
+}
+
+func new_upload_token_api(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	if !r.Form.Has("token") {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	token := r.FormValue("token")
+
+	var new_token string
+	row := db.QueryRow("update accounts set upload_token=uuid_generate_v4() where token=$1 returning upload_token", token)
+	if row.Scan(&new_token) == sql.ErrNoRows {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+
+	fmt.Fprintln(w, new_token)
 }
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/dchest/uniuri"
 	"github.com/gorilla/mux"
@@ -21,6 +22,13 @@ const DATA_FOLDER = "/app/data/"
 const FILE_NAME_LENGTH = 10
 const MAX_UPLOAD_SIZE = 1024 * 1024 * 100
 
+type User struct {
+	Upload_token string
+	Token        string
+	Id           int
+	Account_type string
+}
+
 func main() {
 	db, err := sql.Open("postgres", os.Getenv("POSTGRES_CONN"))
 	if err != nil {
@@ -30,15 +38,12 @@ func main() {
 
 	db.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`)
 	db.Exec("CREATE TABLE IF NOT EXISTS public.images (file_name varchar NOT NULL, created_date timestamptz NOT NULL DEFAULT now(), file_owner int4 NOT NULL, CONSTRAINT images_un UNIQUE (file_name));")
-	db.Exec("CREATE TABLE IF NOT EXISTS  public.accounts (upload_token uuid NOT NULL DEFAULT uuid_generate_v4(), id serial4 NOT NULL, CONSTRAINT accounts_pk PRIMARY KEY (id), CONSTRAINT accounts_un UNIQUE (upload_token));")
+	db.Exec("CREATE TABLE IF NOT EXISTS public.accounts (token uuid NOT NULL DEFAULT uuid_generate_v4(), upload_token uuid NOT NULL DEFAULT uuid_generate_v4(), id serial4 NOT NULL, account_type text NOT NULL DEFAULT 'USER', CONSTRAINT accounts_pk PRIMARY KEY (id), CONSTRAINT accounts_un UNIQUE (upload_token));")
 
-	go auto_deletion()
+	go auto_deletion(db)
 
 	r := mux.NewRouter()
-	r.PathPrefix("/favicon.ico").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "/app/public/favicon.ico")
-	})
-
+	r.PathPrefix("/public").Handler(http.StripPrefix("/public", http.FileServer(http.Dir("/app/public"))))
 	r.PathPrefix("/").Handler(http.StripPrefix("/", middleware(http.FileServer(http.Dir(DATA_FOLDER)), db)))
 
 	fmt.Println("Starting server")
@@ -47,11 +52,21 @@ func main() {
 
 func middleware(h http.Handler, db *sql.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
+		if r.Method == "POST" && strings.HasPrefix(r.URL.Path, "api/") {
 			api(w, r, db)
 			return
 		} else if r.URL.Path == "" {
 			tmpl, err := template.New("index.html").ParseFiles("/app/template/index.html")
+
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+
+			tmpl.Execute(w, r.Host)
+			return
+		} else if r.URL.Path == "api_list" {
+			tmpl, err := template.New("api_list.html").ParseFiles("/app/template/api_list.html")
 
 			if err != nil {
 				log.Fatal(err)

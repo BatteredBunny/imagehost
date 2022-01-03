8d512c61bed4b13080ae1872831ca727349a97d5
Ayes <52951851+ayes-web@users.noreply.github.com>
2022-01-04 00:28:38 +0200

    uses golang logging system and added support back for local storage

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,6 @@ COPY go.mod .
 COPY go.sum .
 RUN go mod download
 
-COPY example.json ./config.json
 COPY public/ public/
 COPY template/ template/
 COPY auto_deletion.go .
@@ -19,4 +18,4 @@ COPY admin.go .
 RUN go build -ldflags "-s -w" -o ./imagehost
 RUN rm go.mod go.sum main.go auto_deletion.go api.go admin.go
 
-ENTRYPOINT [ "/app/imagehost" ]
+ENTRYPOINT [ "/app/imagehost -c /app/config.json" ]
--- auto_deletion.go
+++ auto_deletion.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
-	"time"
+	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -12,11 +12,11 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-func auto_deletion(db *sql.DB, config Config, s3client *s3.S3) {
+func auto_deletion(db *sql.DB, config Config, logger *log.Logger) {
 	c := cron.New()
 
 	c.AddFunc("@hourly", func() {
-		fmt.Printf("%s | Starting hourly cron job\n", time.Now().Format(time.RFC3339))
+		logger.Println("Starting hourly cron job")
 
 		rows, err := db.Query("SELECT file_name FROM public.images WHERE created_date < NOW() - INTERVAL '7 days'")
 		if err != nil { // Im guessing this happens when it gets no results
@@ -27,15 +27,19 @@ func auto_deletion(db *sql.DB, config Config, s3client *s3.S3) {
 			var file_name string
 			rows.Scan(&file_name)
 
-			s3client.DeleteObject(&s3.DeleteObjectInput{
-				Bucket: aws.String(config.S3.Bucket),
-				Key:    aws.String(file_name),
-			})
+			if config.s3client == nil {
+				os.Remove(config.Data_folder + file_name)
+			} else {
+				config.s3client.DeleteObject(&s3.DeleteObjectInput{
+					Bucket: aws.String(config.S3.Bucket),
+					Key:    aws.String(file_name),
+				})
+			}
 		}
 
 		db.Exec("DELETE FROM public.images WHERE created_date < NOW() - INTERVAL '7 days'")
 	})
 
-	fmt.Printf("%s | Starting auto deletion server\n", time.Now().Format(time.RFC3339))
+	logger.Println("Starting auto deletion server")
 	c.Start()
 }

61125c0db67b41883bf2ced59ac75a4061a59c8a
BatteredBunny <ayes2022@protonmail.com>
2025-06-04 23:08:05 +0300

    fix: Enable CGO since sqlite wants it

--- build.nix
+++ build.nix
@@ -10,8 +10,6 @@ buildGoModule {
     "-w"
   ];
 
-  env.CGO_ENABLED = 0;
-
   meta = {
     description = "Simple imagehost written in Go";
     homepage = "https://github.com/BatteredBunny/imagehost";
